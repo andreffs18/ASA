b419709a33ae48c3dab14d65ef6047bf94220eed
Francisco Baio Dias <xicombd@gmail.com>
2015-03-19 00:45:50 +0000

    Saving distances on Author

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ using namespace std;
 class Author{
 private:
 	vector<int> coauthors;
+	int _distance;
 public:
 	Author(){}
 	int get_coauthor(int i){ return coauthors[i]; }
@@ -32,12 +33,14 @@ public:
 		return (find(coauthors.begin(), coauthors.end(), i) != coauthors.end());
 	}
 	int total_coauthors(){ return coauthors.size(); }
+	int get_distance(){ return _distance; }
+	void set_distance(int value){ _distance = value; }
 };
 
 int n, r;	// n - number of nodes, r - number of vertices (relations)
 int paul;	// number corresponding to the origin of the graph (Paul erdos)
 // global control variable to check if index node was already checked
-bool *visited = new bool[n]; 
+bool *visited = new bool[n];
 // global array of objects "authors"
 vector<Author*> author;
 // global queue "q"
@@ -45,7 +48,7 @@ queue<int> q;
 
 /********************************************************************
 	vector Author handling
-	Wrappers around the vector Author 
+	Wrappers around the vector Author
 *********************************************************************/
 // prints graph state
 void print_graph(){
@@ -53,7 +56,7 @@ void print_graph(){
 		cout << i << ": ";
 		for(int j = 0; j < author[i]->total_coauthors(); j++){
 			cout << author[i]->get_coauthor(j) << " ";
-		} 
+		}
 		cout << endl;
 	}
 }
@@ -67,27 +70,27 @@ void add_vertice(int x, int y){
 }
 // check is author "x" has coauthor "y"
 bool is_connected(int x, int y){
-	return author[x]->has_coauthor(y);
+	return author[x]->has_coauthor(y) || author[y]->has_coauthor(x);
 }
 // set some dependencies
 // -> visited, populated array with all entries = False (control)
 // -> dist, populates array with all entries = 0 (control)
 void set_dependencies(){
 	for(int i = 0; i < n; i++){
-		visited[i] = false; 
+		visited[i] = false;
 		//dist[i] = 0;
 	}
 }
 
 /********************************************************************
 	Queue handling
-	Wrappers around queue "q" 
+	Wrappers around queue "q"
 *********************************************************************/
 // returns true is queue is empty
 bool queue_is_empty(){
 	return q.empty();
 }
-// add int to the queue array 
+// add int to the queue array
 void queue_push(int x){
 	q.push(x);
 	visited[x] = true;
@@ -106,7 +109,7 @@ int main(){
 	// creates amount of authors
 	scanf("%u %u", &n, &r);
 	for(int i = 0; i < n; i++) add_node();
-	
+
 	// scans paul erdos number
 	scanf("%u", &paul); paul -= 1;
 
@@ -114,41 +117,49 @@ int main(){
 	for(int i = 0; i < r; i++){
 		int a, ca;
 		scanf("%u %u", &a, &ca);
+		cout << a << " " << ca << endl;
 		add_vertice(a-1, ca-1);
 	}
 
 	// sets control variables to false, 0, what not
 	set_dependencies();
-	
+
 	// print_graph();
 	// set paul vertice as first in the queue
 	queue_push(paul);
+	author[paul]->set_distance(0);
 
 	// print notification
-	//cout << "BFS starting in vertice " << paul << endl;
+	cout << "BFS starting in vertice " << paul << endl;
+
+	int distance = 0;
 
 	// Unless the queue is empty
 	while (!queue_is_empty()){
 		// Mark and pop the vertex from the queue
 		int v = queue_pop();
 		// display the visited vertices
-		//cout << "Pop from queue " << v << endl;
+		// cout << "Pop from queue " << v << endl;
+
+		distance = 	author[v]->get_distance();
 
 		// From the visited vertex v try to explore all the connected vertice s
 		for(int w = 0; w < n; w++){
 			// if the current v is the same as the one we are searching
 			// or if both are not connected, just skip
-			if(w == v or !is_connected(v, w)) continue;
+			if(w == v || !is_connected(v, w)) continue;
 			// if still not visited, go ahead and add it to the queue
 			if(!visited[w]){
-				cout << "adding " << w << " to queue" << endl;
+				// cout << "adding " << w << " to queue" << endl;
 				queue_push(w);
+				author[w]->set_distance(distance+1);
 				// dict[w] = 1;
 			}
+
 		}
 	}
 
 	// print qtd of nodes p/distance
 	for(int i = 0; i < n; i++)
-		cout << "TODO" << endl;
+		cout << "Author " << i+1 << " - " << author[i]->get_distance() << endl;
 }
