ab47f4f56e8997b2320d6f45b8bf3879c277c45b
André Silva <anndreffs18@gmail.com>
2015-04-24 00:21:06 +0100

    lel

--- 2Entrega/main.cpp
+++ 2Entrega/main.cpp
@@ -75,30 +75,18 @@ public:
 
 };
 
-void printArr(int dist[], int v) {
-  // printf("Vertex Distance from Source\n");
-  for (int i = 0; i < v; ++i) {
-  	if(dist[i] < 0) {
-  		cout << "I" << endl;
-  	}
-  	else if(dist[i] == INT_MAX) {
-	    cout << "U" << endl;
-  	}
-  	else {
-  		cout << dist[i] << endl;
-  	}
-  }
-}
 
 bool BellmanFord(Graph g, int source){
 	int V = g.get_v();
 	int E = g.get_e();
 	int dist[V];
+	int control[v];
 
 	// initialize all values in graph to 0 and INF,
-	for (int i = 0; i < V; i++)
+	for (int i = 0; i < V; i++){
 		dist[i] = INT_MAX;
-
+		control[i] = 0;
+	}
 	dist[source] = 0;
 
 	// go V-1 times and relax vertices
@@ -108,23 +96,34 @@ bool BellmanFord(Graph g, int source){
 			int v = g.get_edges()[j].get_v();
 			int weight = g.get_edges()[j].get_weight();
 
-			//if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
-			if (dist[v] > dist[u] + weight && dist[u] != INT_MAX)
+			if (dist[v] > dist[u] + weight)
 				dist[v] = dist[u] + weight;
+
+			if(i == 1){
+				control[v] = dist[v];
+			}
 		}
 	}
-
-	printArr(dist, V);
 
 	for (int i = 0; i < E; i++){
 		int u = g.get_edges()[i].get_u();
 		int v = g.get_edges()[i].get_v();
 		int weight = g.get_edges()[i].get_weight();
 
-		if (dist[v] > dist[u] + weight && dist[u] != INT_MAX)
-		// if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
-			return false;
+		if (dist[v] > dist[u] + weight){}
+
 	}
+
+	for (int i = 0; i < v; ++i) {
+		if(dist[i] == INT_MAX) {
+			cout << "U" << endl;
+		} else if(control[v] == dist[v]) {
+			cout << "I" << endl;
+		} else {
+			cout << dist[i] << endl;
+		}
+	}
+
 	return true;
 }
 
@@ -145,21 +144,6 @@ int main(){
 		scanf("%u %u %u", &a, &b, &w);
 		g.add_edge(a-1, b-1, w);
 	}
-
-	// g.print();
-
+	// runs Bellman Ford algorithm
 	BellmanFord(g, source);
-
-	// cout << result;
-	// scans #r relations of coauthors
-	//InitializeSingleSource(graph, source);
-	//BellmanFord(graph, wheight, source);
-
-	// print qtd of nodes p/distance
-	// for(int i = 1, c = 1; i < n; i++, c++){
-	// 	if(i+1 > n || dist[i] != dist[i+1]){
-	// 		printf("%d\n", c);
-	// 		c = 0;
-	// 	}
-	// }
 }
